6940f97d5cd89544113349f9b677ad59b720d7f9
dekanymate <dekanymate@gmail.com>
2022-03-07 12:22:34 +0100

    3. név deklarálása

--- EgyeniKonfliktusKezeles/src/egyenikonfliktuskezeles/EgyeniKonfliktusKezeles.java
+++ EgyeniKonfliktusKezeles/src/egyenikonfliktuskezeles/EgyeniKonfliktusKezeles.java
@@ -2,7 +2,8 @@ package egyenikonfliktuskezeles;
 public class EgyeniKonfliktusKezeles {
     public static void main(String[] args) {
         String nev1 = "DM";
-        String nev2 = "x","y";
+        String nev2 = "xy";
+        String nev3;
 
         
     }
