3bb10c7effa782bb27a1b72c9b0f6c710ac5b8e8
dekanymate <dekanymate@gmail.com>
2022-03-07 12:19:11 +0100

    konfiktus kezelve: 1. és 2. név inicializálva

--- EgyeniKonfliktusKezeles/src/egyenikonfliktuskezeles/EgyeniKonfliktusKezeles.java
+++ EgyeniKonfliktusKezeles/src/egyenikonfliktuskezeles/EgyeniKonfliktusKezeles.java
@@ -1,12 +1,10 @@
 package egyenikonfliktuskezeles;
 public class EgyeniKonfliktusKezeles {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        String nev1;
-        String nev2;
-=======
-        String nev1 = "x","y";
->>>>>>> c1361055454250b03e8bbfc8b84b972964ff0473
+        String nev1 = "DM";
+        String nev2 = "x","y";
+
+        
     }
     
 }
