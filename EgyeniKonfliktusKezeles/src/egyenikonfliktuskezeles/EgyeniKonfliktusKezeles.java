b44bfdaf0e1d10e00d7d7844a197b61728d60813
Mate Dekany <96132472+dekanymate@users.noreply.github.com>
2022-03-07 12:24:30 +0100

    3. név deklarálása

--- EgyeniKonfliktusKezeles/src/egyenikonfliktuskezeles/EgyeniKonfliktusKezeles.java
+++ EgyeniKonfliktusKezeles/src/egyenikonfliktuskezeles/EgyeniKonfliktusKezeles.java
@@ -3,6 +3,7 @@ public class EgyeniKonfliktusKezeles {
     public static void main(String[] args) {
         String nev1 = "DM";
         String nev2 = "x","y";
+        int nev3;
 
         
     }
