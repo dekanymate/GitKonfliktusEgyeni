c1361055454250b03e8bbfc8b84b972964ff0473
Mate Dekany <96132472+dekanymate@users.noreply.github.com>
2022-03-07 12:13:18 +0100

    nev inicializalasa

--- EgyeniKonfliktusKezeles/src/egyenikonfliktuskezeles/EgyeniKonfliktusKezeles.java
+++ EgyeniKonfliktusKezeles/src/egyenikonfliktuskezeles/EgyeniKonfliktusKezeles.java
@@ -1,7 +1,7 @@
 package egyenikonfliktuskezeles;
 public class EgyeniKonfliktusKezeles {
     public static void main(String[] args) {
-        String nev1;
+        String nev1 = "x","y";
     }
     
 }
